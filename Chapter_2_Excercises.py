101138c9ac22602c393e7a7cbffa47efb6a5b136
Floopy <Theamazingfloopy@gmail.com>
2025-02-01 23:52:54 -0800

    done

---
+++ Chapter_2_Excercises.py
@@ -0,0 +1,20 @@
+
+#Part 1
+import math
+radius = 5
+volume = (4/3)*(math.pi)*radius**3
+print(volume) # in cubic centimeters
+
+# part 2
+x = 42
+xsin = math.cos(x)
+xcos = math.sin(x)
+ruletrue = (xsin)**2 + (xcos)**2
+print(ruletrue)
+
+# Part 3
+print(math.e**2)
+
+print(math.pow(math.e, 2))
+
+print(math.exp(2))
